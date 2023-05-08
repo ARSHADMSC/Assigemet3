611458124b4cf6dabd1590b27bbbfa612820cad7
ARSHADMSC <91111692+ARSHADMSC@users.noreply.github.com>
2023-05-08 13:08:43 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,9 @@
+function Home(){
+    return(
+      <div classname="homee">
+        <h1 id="head-1">Welcome to BankApplication</h1>
+        <img src="bank.jpeg"></img>
+      </div>
+    )
+  }
+  
